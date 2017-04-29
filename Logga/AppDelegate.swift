2a0b88c288269a7e2c3815db287d4507531fa596
Jerry S <udensi.jerry@gmail.com>
2017-04-29 20:48:57 +0800

    Major changes. new_window is now master for window proposals

--- Logga/AppDelegate.swift
+++ Logga/AppDelegate.swift
@@ -13,36 +13,18 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
 
-    @IBOutlet weak var sView: NSView!
-    
-    @IBOutlet weak var dbs: NSButton!
-    var mdGuy = modalGUy(windowNibName: "modalGUy");
-
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
-        sView.layer?.backgroundColor = NSColor.clearColor().CGColor
         self.window!.backgroundColor = NSColor(patternImage: NSImage(named: "collec.png")!)
-        
-//        dbs.col = NSColor(patternImage: NSImage(named: "btn.png")!).CGColor
-        
-        let pstyle = NSMutableParagraphStyle()
-        pstyle.alignment = .Center
-        
-        dbs.attributedTitle = NSAttributedString(string: "Better Login", attributes: [ NSForegroundColorAttributeName : NSColor.whiteColor(), NSParagraphStyleAttributeName : pstyle ])
-        
         
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
     }
-
-    @IBAction func light(sender: AnyObject) {
-       // mdGuy.beginSheet(self.window)
-//        mdGuy.showWindow(self.window)
-    }
-    @IBAction func prefGuy(sender: AnyObject) {
-        mdGuy.beginSheet(self.window)
+    
+    func applicationShouldTerminateAfterLastWindowClosed(sender: NSApplication) -> Bool {
+        return true
     }
     
 }
